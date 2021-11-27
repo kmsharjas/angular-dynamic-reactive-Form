340c111c7869e158aab08dc71adf6372f5d3b783
SHARJAS KM <sharjas.k.m@gmail.com>
2021-11-27 17:43:45 +0530

    demo

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray, Validators, FormGroup } from '@angular/forms';
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent implements OnInit {
+
+  public dynamicForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.dynamicForm = this.fb.group({
+      row: this.fb.array([this.addRowGroup()])
+    });
+  }
+
+  private addRowGroup(): FormGroup {
+    return this.fb.group({
+      category: [null, Validators.required],
+      Subcategory: [null, Validators.required],
+      Units: [null, Validators.required],
+    });
+  }
+
+  addRow(): void {
+    if (this.rowArray.valid) {
+      this.rowArray.push(this.addRowGroup());
+      console.log(this.rowArray);
+    }
+  }
+
+  removeAddress(index: number): void {
+    this.rowArray.removeAt(index);
+  }
+
+  get rowArray(): FormArray {
+    return <FormArray>this.dynamicForm.get('row');
+  }
+
+  ngOnInit(): void {
+  }
+  Submit(val){
+console.log(val.value);
+
+  }
+}
